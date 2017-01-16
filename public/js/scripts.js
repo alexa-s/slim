ab00abf1d702944ae8b41c77756a40fa1d6760ef
Alexandra Stanciu <alexa.stanciu2@gmail.com>
2017-01-17 00:33:04 +0200

    disabled right click

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
+
     $('.carousel-item:eq(0)').addClass('active');
     $('#carouselControls').carousel({
         interval: 5000
     });
 
-    $("img").on("contextmenu",function(){
-       return false;
-    }); 
+
 });
